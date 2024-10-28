1aded5784e7999a5e736219c87a19243f6bf4a20
Christophe <christophegauthiersokpoly@gmail.com>
2024-10-28 14:28:43 +0100

    update

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-        <span></span>
-        <button className='lng'>en</button>
-        <button className='lng'>fr</button>
+
 
 SEed77ht frcrmz
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,10 +15,10 @@ const Footer = () => {
 
   return (
     <section className='footer'>
-      <div className='logo_footer'>
-        <img src={logo} alt='' width="80px" />
-      </div>
-      <div className='navs'>
+      <div className='footer1'>
+        <div className='logo_footer'>
+          <img src={logo} alt='' width="80px" />
+        </div>
         <ul className='nav'>
           <NavLink to='/' onClick={scrollToTop}><li>Accueil</li></NavLink>
           <NavLink to='/about' onClick={scrollToTop}><li>A propos</li></NavLink>
@@ -26,30 +26,32 @@ const Footer = () => {
           <NavLink to='/actions' onClick={scrollToTop}><li>Nos actions</li></NavLink>
           <NavLink to='/contact' onClick={scrollToTop}><li>Contacts</li></NavLink>
         </ul>
-        <p>contact@magicrecre.com</p>
-        <p>&copy; October 2024 Magic Récré, All Rights Reserved.<br/> Developed by SOKPOLY Christophe.</p>
+        <div className='social_media'>
+          <div>
+            <a href="https://www.facebook.com/magicrecre?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.instagram.com/magicrecre/profilecard/?igsh=emozcWxyN3praDBl" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.tiktok.com/@magicrecre?_t=8qZfYTzE64s&_r=1" target="_blank" rel="noopener noreferrer">
+              <FaTiktok />
+            </a>
+          </div>
+          <div>
+            <a href="" target="_blank" rel="noopener noreferrer">
+              <FaXTwitter />
+            </a>
+          </div>
+        </div>
       </div>
-      <div className='social_media'>
-        <div>
-          <a href="https://www.facebook.com/magicrecre?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
-            <FaFacebookF />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.instagram.com/magicrecre/profilecard/?igsh=emozcWxyN3praDBl" target="_blank" rel="noopener noreferrer">
-            <FaInstagram />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.tiktok.com/@magicrecre?_t=8qZfYTzE64s&_r=1" target="_blank" rel="noopener noreferrer">
-            <FaTiktok />
-          </a>
-        </div>
-        <div>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <FaXTwitter />
-          </a>
-        </div>
+      <div className='footer2'>
+        <p>contact@magicrecre.com</p>
+        <p>&copy; October 2024 Magic Récré, All Rights Reserved. Developed by SOKPOLY Christophe.</p>
       </div>
     </section>
   );
