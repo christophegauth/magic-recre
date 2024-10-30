40d1275ad4db82cd9c3fe608d8a7d0224f8716c4
Christophe <christophegauthiersokpoly@gmail.com>
2024-10-30 16:48:10 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
-import { NavLink} from 'react-router-dom'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
-
-  const [Mobile, setMobile] = useState(false)
+  const [Mobile, setMobile] = useState(false);
 
   return (
     <nav className='navbar'>
-      <img src={logo} alt='' width="80px"/>
-      <ul className={Mobile? "nav-links-mobile" : "nav-links"} onClick={() =>setMobile(false)}>
+      <img src={logo} alt='logo' width="80px"/>
+      <ul className={Mobile ? "nav-links-mobile show" : "nav-links"} onClick={() => setMobile(false)}>
         <NavLink to='/'><li>Accueil</li></NavLink>
         <NavLink to='/about'><li>A propos</li></NavLink>
         <NavLink to='/galerie'><li>Galerie</li></NavLink>
@@ -24,12 +23,11 @@ const Navbar = () => {
           <button className='lng'>fr</button>
         </div>
       </ul>
-      <button className='mobile-menu-icon' onClick={() =>setMobile(!Mobile)}>
-        {Mobile ? <ImCross/> : <FaBars />}
+      <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
+        {Mobile ? <ImCross /> : <FaBars />}
       </button>
     </nav>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
