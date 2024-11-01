f0fc117185413ee25b9b0c089da3fdda260d880c
Christophe <christophegauthiersokpoly@gmail.com>
2024-11-01 16:25:34 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 import { FaBars } from "react-icons/fa";
@@ -7,6 +7,7 @@ import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
   const [Mobile, setMobile] = useState(false);
+  const location = useLocation(); 
 
   useEffect(() => {
     if (Mobile) {
@@ -14,23 +15,54 @@ const Navbar = () => {
     } else {
       document.body.classList.remove('no-scroll');
     }
-
-    // Supprimer la classe 'no-scroll' si le composant est démonté
     return () => document.body.classList.remove('no-scroll');
   }, [Mobile]);
+
+  const getMobileLinksClass = () => {
+    switch(location.pathname) {
+      case '/':
+        return 'mobile-links-home';
+      case '/about':
+        return 'mobile-links-about';
+      case '/galerie':
+        return 'mobile-links-galerie';
+      case '/actions':
+        return 'mobile-links-actions';
+      default:
+        return '';
+    }
+  };
+
+  const getLngClass = () => {
+    switch(location.pathname) {
+      case '/':
+        return 'lng-home';
+      case '/about':
+        return 'lng-about';
+      case '/galerie':
+        return 'lng-galerie';
+      case '/actions':
+        return 'lng-actions';
+      default:
+        return '';
+    }
+  };
 
   return (
     <nav className='navbar'>
       <img src={logo} alt='logo' width="80px"/>
-      <ul className={Mobile ? "nav-links-mobile show" : "nav-links"} onClick={() => setMobile(false)}>
+      <ul 
+        className={`${Mobile ? "nav-links-mobile show" : "nav-links"} ${getMobileLinksClass()}`} 
+        onClick={() => setMobile(false)}
+      >
         <NavLink to='/'><li>Accueil</li></NavLink>
         <NavLink to='/about'><li>A propos</li></NavLink>
         <NavLink to='/galerie'><li>Galerie</li></NavLink>
         <NavLink to='/actions'><li>Nos actions</li></NavLink>
         <span></span>
         <div>
-          <button className='lng'>en</button>
-          <button className='lng'>fr</button>
+          <button className={`lng ${getLngClass()}`}>en</button>
+          <button className={`lng ${getLngClass()}`}>fr</button>
         </div>
       </ul>
       <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
